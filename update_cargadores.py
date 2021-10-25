2301c96a49212eae925856b5eb324fb5ac82fdbd
Bruno Stefoni <brunostefon12@gmail.com>
2021-10-24 23:42:33 -0400

    query optimizada

--- update_cargadores.py
+++ update_cargadores.py
@@ -30,40 +30,60 @@ def mantener_log():
 
 
 def query_data_diaria(fecha_str_ayer, fecha_str_hoy):
-    # Query que ejecutara el script cada dia a las 7am
-    query_ = ( f"""
+    # Query entrega 24 horas de data telemetria de cargadores a partir de las 7:30am de un dia
+    db1 = MySQLdb.connect(host="192.168.11.150",
+                          user="brunom",
+                          passwd="Manzana",
+                          db="stp_estacionamiento")
+
+    cur1 = db1.cursor()
+
+    query1 = ( f"""
                 SELECT * FROM (
-                    SELECT 
+                    SELECT
                         min(id) AS id, marquesina_id, marquesina_nombre, cargador_id, cargador_nombre,
                         pistola_id, pistola_nro, power_active_import, energy_active_import_register,
-                        voltage, fecha_hora_evento, min(fecha_hora_consulta) AS fecha_hora_consulta, 
-                        soc, temperatura, current_import, fecha_hora_inicio_valores_copec, 
+                        voltage, fecha_hora_evento, min(fecha_hora_consulta) AS fecha_hora_consulta,
+                        soc, temperatura, current_import, fecha_hora_inicio_valores_copec,
                         fecha_hora_termino_valores_copec
                     FROM
                         stp_estacionamiento.cargadores_historico
-                    WHERE 
-                        fecha_hora_evento > '{fecha_str_ayer} 07:30:00' AND
-                        fecha_hora_evento < '{fecha_str_hoy} 07:30:01' AND
-                        power_active_import > '20' AND voltage<>'0' AND current_import <>'0' AND soc <= '99'
-                    GROUP BY 
+                    WHERE
+                        power_active_import > '20' AND
+                        voltage <> '0' AND
+                        current_import <> '0' AND
+                        soc <= '99' AND
+                        fecha_hora_evento BETWEEN '{fecha_str_ayer} 07:30:01' AND '{fecha_str_hoy} 07:30:00'
+                    GROUP BY
                         marquesina_id, marquesina_nombre, cargador_id, cargador_nombre,
                         pistola_id, pistola_nro, power_active_import, energy_active_import_register,
                         voltage, fecha_hora_evento,  soc, temperatura, current_import,
                         fecha_hora_inicio_valores_copec, fecha_hora_termino_valores_copec
                 ) AS CHF
-                LEFT JOIN 
+                LEFT JOIN
                 (
-                    SELECT 
+                    SELECT
                         copeq_id AS pistola_id_copec, id AS pistola_id_stp, pistola_nombre
-                    FROM 
+                    FROM
                         stp_estacionamiento.pistolas
                 ) AS DICT
-                ON CHF.pistola_id = DICT.pistola_id_copec
-                """ )
+                ON CHF.pistola_id = DICT.pistola_id_copec;
+                """
+             )
+
+    cur1.execute(query1)
+    df1_ = pd.DataFrame([row for row in cur1.fetchall() if row[0] is not None],
+                        columns=[i[0] for i in cur1.description])
+
+    cur1.close()
+    db1.close()
+
+    return df1_
+
 
 
 def query_reservas_diaria(fecha_str_ayer, fecha_str_hoy):
-    # Query entrega tabla reservas para cada dia
+    # Query entrega 24 horas de data reservas a partir de las 7:30am de un dia
     db0 = MySQLdb.connect(host="192.168.11.150",
                           user="brunom",
                           passwd="Manzana",
@@ -72,22 +92,24 @@ def query_reservas_diaria(fecha_str_ayer, fecha_str_hoy):
     cur0 = db0.cursor()
 
     query0 = ( f"""
-                SELECT pistola_id AS pistola_id_stp2, patente, fecha_hora_reserva, 
-                      usuario_id, usuario_inicio_id, usuario_termino_id 
-                FROM stp_estacionamiento.reservas 
+                SELECT
+                    pistola_id AS pistola_id_stp2, patente, fecha_hora_reserva, 
+                    usuario_id, usuario_inicio_id, usuario_termino_id 
+                FROM 
+                    stp_estacionamiento.reservas 
                 WHERE 
-                    fecha_hora_reserva > '{fecha_str_ayer} 07:30:00' AND
-                    fecha_hora_reserva < '{fecha_str_hoy} 07:30:01';
-                """ )
+                    fecha_hora_reserva BETWEEN '{fecha_str_ayer} 07:30:01' AND '{fecha_str_hoy} 07:30:00';
+                """
+             )
 
     cur0.execute(query0)
-    df_ = pd.DataFrame([row for row in cur0.fetchall() if row[0] is not None],
-                       columns=[i[0] for i in cur0.description])
+    df0_ = pd.DataFrame([row for row in cur0.fetchall() if row[0] is not None],
+                        columns=[i[0] for i in cur0.description])
 
     cur0.close()
     db0.close()
 
-    return df_
+    return df0_
 
 
 def main():
@@ -96,9 +118,12 @@ def main():
     fecha_ayer = fecha_hoy - timedelta(days=1)
     fecha_hoy = fecha_hoy.strftime('%Y-%m-%d')
     fecha_ayer = fecha_ayer.strftime('%Y-%m-%d')
+
     logger.info(f"Primer intento con fecha_ayer={fecha_ayer} y fecha_hoy={fecha_hoy}")
     df_res = query_reservas_diaria(fecha_ayer, fecha_hoy)
-    df_res.to_excel('df.xlsx', index=False)
+    df = query_data_diaria(fecha_ayer, fecha_hoy)
+    df_res.to_parquet('df_res.parquet', compression='gzip')
+    df.to_parquet('df.parquet', compression='gzip')
 
 
 if __name__ == '__main__':
